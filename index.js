f401dc7a4e3fcc1261e8b280ad5a96e7eed3b985
avuillermot <avuillermot@hotmail.com>
2017-11-17 21:22:45 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+/*var moment = require('moment');
+var exec = require("child_process").exec;
+var url = require("url");
+var https = require('https');*/
+var http = require('http');
+/*var fs = require('fs');
+var httpConfig = require("./config/http");
+var express = require("express");
+var bodyParser = require('body-parser');
+var q = require('q');
+
+var sUsers = require("./services/user");
+var sPassport = require("./services/passport");
+
+
+require("./api/apiAuthenticate");
+require("./api/apiMessageBird");*/
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+//var port = process.env.PORT || 80;
+server.listen(80);
+
