eb21b8ae175e3eee0d8be6743751997c35ce898c
avuillermot <avuillermot@hotmail.com>
2017-11-17 21:42:07 +0100

    file

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ var url = require("url");
 var https = require('https');
 var http = require('http');
 var fs = require('fs');
-var httpConfig = require("./config/http");
+//var httpConfig = require("./config/http");
 var express = require("express");
 var bodyParser = require('body-parser');
 var q = require('q');
 
-var sUsers = require("./services/user");
-var sPassport = require("./services/passport");
+//var sUsers = require("./services/user");
+//var sPassport = require("./services/passport");
 
 
-require("./api/apiAuthenticate");
-require("./api/apiMessageBird");
+//require("./api/apiAuthenticate");
+//require("./api/apiMessageBird");
 
 var server = http.createServer(function(request, response) {
 
