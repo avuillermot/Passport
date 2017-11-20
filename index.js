0f1e8f69925391101bff10b786d1c15409150721
avuillermot <avuillermot@hotmail.com>
2017-11-20 16:51:11 +0100

    http

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ app.put('/:module/password', function(req, res){
 	}
 	else httpConfig.callback(400,{},res);
 });
-/*
+
 app.get('/:module', function(request, response){
 	if (request.body == null && req.body != undefined)  {
 		var context = httpConfig.getAuthorizationContext(request);
@@ -189,9 +189,9 @@ app.get('/test', function(request, response){
 	response.writeHead(200, {"Content-Type": "text/plain"});
 	response.end("Hello World!");
 });
-*/
+
 var port = process.env.PORT || 1337;
 var options = {
 };
-https.createServer(options, app).listen(httpConfig.portConfig);
+http.createServer(app).listen(httpConfig.portConfig);
 console.log("end");
