2801174a14cd25edc656231a0a146bc40dd3471c
avuillermot <avuillermot@hotmail.com>
2017-11-21 20:13:53 +0100

    connection

--- config/bdd.js
+++ config/bdd.js
@@ -7,9 +7,9 @@ var poolConfig = {
 var configuration = {
     "mssql": {		
 		"driver": "tedious",
-		"userName": "sa",
-		"password": "Kerenann06041979",
-		"server": "dev-takedoc.cloudapp.net", // You can use 'localhost\\instance' to connect to named instance 
+		"userName": "carl",
+		"password": "wxcvbn123",
+		"server": "DEV-TAKEDOC", // You can use 'localhost\\instance' to connect to named instance 
 
 		"options": {
 			//"instanceName": "sqlexpress",
