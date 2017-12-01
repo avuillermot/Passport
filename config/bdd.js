b041bfc7c1c6490fcf5320c5b89e596095d3c83d
avuillermot <avuillermot@hotmail.com>
2017-12-01 17:01:54 +0100

    ok

--- config/bdd.js
+++ config/bdd.js
@@ -7,14 +7,14 @@ var poolConfig = {
 var configuration = {
     "mssql": {		
 		"driver": "tedious",
-		"userName": "carl",
-		"password": "Kerenann06041979",
-		"server": "front-carl.westeurope.cloudapp.azure.com", // You can use 'localhost\\instance' to connect to named instance 
-		"options": {
-			"port": 49363,
-			"database": "PASSPORT",
-			"encrypt": false
-			//"instancename": 'SQLEXPRESS'
+		userName: "carl",
+		password: "Kerenann06041979",
+		server: "front-carl.westeurope.cloudapp.azure.com", // You can use 'localhost\\instance' to connect to named instance 
+		options: {
+			port: 49363,
+			database: "PASSPORT",
+			encrypt: false
+			//instancename: 'SQLEXPRESS'
 		}
 	}
 }
