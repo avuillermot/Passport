f763555a0169819c4eb9c763ad2653bc3a11befd
avuillermot <avuillermot@hotmail.com>
2017-12-01 16:30:42 +0100

    name

--- config/bdd.js
+++ config/bdd.js
@@ -9,7 +9,7 @@ var configuration = {
 		"driver": "tedious",
 		"userName": "carl",
 		"password": "Kerenann06041979",
-		"server": "13.95.123.103", // You can use 'localhost\\instance' to connect to named instance 
+		"server": "Front-carl", // You can use 'localhost\\instance' to connect to named instance 
 		"database": "PASSPORT",
 		"options": {
 			//"instanceName": "SQLEXPRESS"
