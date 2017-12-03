8c777914b9dce92b52c075327b461844efaf45f2
avuillermot <avuillermot@hotmail.com>
2017-12-03 21:46:12 +0100

    name

--- config/bdd.js
+++ config/bdd.js
@@ -9,8 +9,9 @@ var configuration = {
 		driver: "tedious",
 		userName: "carl",
 		password: "Kerenann06041979",
+		server: "front-carl",
 		//server: "front-carl.westeurope.cloudapp.azure.com", // You can use 'localhost\\instance' to connect to named instance 
-		server: "52.175.166.28",
+		//server: "52.175.166.28",
 		options: {
 			//port: 49363,
 			database: "PASSPORT",
