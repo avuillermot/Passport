b4c00ddfa2c66349a606ad4b7e9b383de62bdb19
avuillermot <avuillermot@hotmail.com>
2017-12-03 21:15:32 +0100

    ip

--- config/bdd.js
+++ config/bdd.js
@@ -5,11 +5,11 @@ var poolConfig = {
 };
 
 var configuration = {
-    "mssql": {		
-		"driver": "tedious",
+    mssql: {		
+		driver: "tedious",
 		userName: "carl",
 		password: "Kerenann06041979",
-		server: "front-carl.westeurope.cloudapp.azure.com", // You can use 'localhost\\instance' to connect to named instance 
+		server: "52.178.116.28", // You can use 'localhost\\instance' to connect to named instance 
 		options: {
 			port: 49363,
 			database: "PASSPORT",
